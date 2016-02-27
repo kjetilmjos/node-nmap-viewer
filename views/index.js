17006f640fa113cb45902834b7a95b05e0cc6b2f
Kjetil <kjetil.mjos@gmail.com>
2016-02-27 22:46:00 +0100

    Added files via upload

--- views/index.js
+++ views/index.js
@@ -1,3 +1,6 @@
-function test(){
-  alert("nisssa");
+function changedescription(ip) {
+  var description = prompt("Enter new description for IP " + ip);
+  if (description != null) {
+    alert(description);
+  }
 }
