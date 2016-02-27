b376596bdc6eed615850936ed09a1d6a45aa1af2
Kjetil <kjetil.mjos@gmail.com>
2016-02-27 20:51:16 +0100

    Added files via upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+//var app = require('express')();
+var express  = require('express');
+var app      = express();
 var http = require('http').Server(app);
 var mongoose = require('mongoose');
 var models = require('./schemas');
 mongoose.connect("localhost:27017/node_npm_viewer");
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+app.use('/static', express.static('views'));
 
 http.listen(3001, function() {
 
