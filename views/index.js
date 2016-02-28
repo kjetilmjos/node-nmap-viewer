d40ba7aaed19ddd7b43d67645ff332db3510d6d6
Kjetil <kjetil.mjos@gmail.com>
2016-02-28 14:16:06 +0100

    Added files via upload

--- views/index.js
+++ views/index.js
@@ -1,6 +1,16 @@
 function changedescription(ip) {
   var description = prompt("Enter new description for IP " + ip);
   if (description != null) {
-    alert(description);
+    return $.ajax({
+   url: "/changedesc",
+   type: "POST",
+   data: {
+     "ip": ip,
+     "description": description,
+   },
+   success: function() {
+     location.reload();
+   }
+ });
   }
 }
